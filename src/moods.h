4ff4d364c0b5a973ed4f629fb0d0361ac353da45
Janne H. Korhonen <janne.h.korhonen@helsinki.fi>
2015-04-15 17:47:26 +0300

    Started cleaning up C++ code

---
+++ src/moods.h
@@ -0,0 +1,14 @@
+
+#include <vector>
+#include <iostream>
+#include <cfloat>
+#include <stdint.h>
+
+
+// for data about position in a sequence
+typedef unsigned long long position_t;
+
+
+// matrix scores
+typedef double score_t;
+typedef std::vector<std::vector<score_t>> score_matrix;
