ff58eed526e35e62f87f6f34afa4029ec8b2ce87
Janne H. Korhonen <janne.h.korhonen@helsinki.fi>
2015-10-26 14:43:34 +0200

    Temp build script for python stuff

---
+++ python/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+swig -c++ -python -outdir MOODS/ core/scan.i
+swig -c++ -python -outdir MOODS/ core/tools.i
+swig -c++ -python -outdir MOODS/ core/misc.i
+python setup.py build_ext --inplace
