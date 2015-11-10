2e9922f8c4fba0f5b6714e4a54a8d17772707fce
Janne H. Korhonen <janne.h.korhonen@helsinki.fi>
2015-11-10 14:03:12 +0200

    Updated example scripts

--- python/ex-basic-usage.py
+++ python/ex-basic-usage.py
@@ -2,24 +2,27 @@
 
 import MOODS.scan
 import MOODS.tools
+import MOODS.parsers
 
 import sys
 import time
 import os
 
-def load_matrix(filename):
-    with open(filename, "r") as file_handle:
-        mat = [[float(i) for i in line.split()] for line in file_handle]
-        return mat
-
-
 total = time.clock()
 start = time.clock()
 
 if len(sys.argv) < 3:
-	print "usage: python ex-basic-usage.py [sequence file] [matrix directory]"
-	sys.exit(1)
+    print "usage: python ex-basic-usage.py [sequence file] [matrix directory]"
+    sys.exit(1)
 
+
+# ---- parameters ----
+pseudocount = 0.00001
+pvalue = 0.0001
+# ---- end parameters ----
+
+
+# ---- read sequence ----
 # read an sequence
 # in this example we do not do any fancy input parsing,
 # so the file should have only characters ACGT
@@ -31,22 +34,10 @@ with open(file_name, "r") as file_handle:
 end = time.clock()
 
 print "Reading sequence:", end - start
+# ---- end read sequence ----
 
-start = time.clock()
 
-# read the matrix files
-# should be in JASPAR .pfm format
-matrix_directory = sys.argv[2]
-matrix_names = [filename for filename in os.listdir(matrix_directory) if filename[-4:] == '.pfm']
-
-matrices = [load_matrix(matrix_directory + filename) for filename in matrix_names]
-# reverse complements
-rc_matrices = [MOODS.tools.reverse_complement(m) for m in matrices]
-
-end = time.clock()
-
-print "Reading matrices:", end - start
-
+# ---- compute bg ----
 start = time.clock()
 
 # estimate background from the sequence file
@@ -55,40 +46,62 @@ bg = MOODS.tools.bg_from_sequence_dna(seq,1)
 end = time.clock()
 
 print "Computing background:", end - start
+# ---- end compute bg ----
 
-# log-odds transformation for matrices
-log_odds_matrices = [MOODS.tools.log_odds(m, bg, 1) for m in matrices] + [MOODS.tools.log_odds(m, bg, 1) for m in rc_matrices]
-# thresholds computed from p-value
-thresholds = [MOODS.tools.threshold_from_p(m, bg, 0.0001) for m in log_odds_matrices]
 
+# ---- process matrices ----
 start = time.clock()
 
-# scanning
-results = MOODS.scan.scan_dna(seq, log_odds_matrices, bg, thresholds, 7)
+# read the matrix files
+# should be in JASPAR .pfm format
+matrix_directory = sys.argv[2]
+matrix_names = [filename for filename in os.listdir(matrix_directory) if filename[-4:] == '.pfm']
 
-# separate reverse complements and the non-reverse complements 
-fr = results[:len(matrices)]
-rr = results[len(matrices):]
+matrices = [MOODS.parsers.pfm_log_odds(matrix_directory + filename, bg, pseudocount) for filename in matrix_names]
+# reverse complements
+matrices = matrices + [MOODS.parsers.pfm_log_odds_rc(matrix_directory + filename, bg, pseudocount) for filename in matrix_names]
 
-# mix the results together, use + and - to indicate strand
-results = [ [(r.pos, r.score, '+') for r in fr[i]] + [(r.pos, r.score, '-') for r in rr[i]] for i in xrange(len(matrices))]
+thresholds = [MOODS.tools.threshold_from_p(m, bg, pvalue) for m in matrices]
 
 
 end = time.clock()
 
+print "Reading matrices:", end - start
+# ---- end process matrices ----
+
+
+# ---- scanning ----
+start = time.clock()
+results = MOODS.scan.scan_dna(seq, matrices, bg, thresholds, 7)
+end = time.clock()
+
 print "Scanning:", end - start
+
+# ---- end scanning ----
+
+# ---- process results ----
+# separate reverse complements and the non-reverse complements 
+fr = results[:len(matrix_names)]
+rr = results[len(matrix_names):]
+
+# mix the results together, use + and - to indicate strand
+results = [ [(r.pos, r.score, '+') for r in fr[i]] + [(r.pos, r.score, '-') for r in rr[i]] for i in xrange(len(matrix_names))]
+
+
 
 print "Total hits:", sum([len(r) for r in results])
 
-for (matrix,matrix_name,result) in zip(matrices, matrix_names, results):
-    l = len(matrix[0])
-    for r in sorted(result, key=lambda r: r[0]):
-        strand = r[2]
-        pos = r[0]
-        hitseq = seq[pos:pos+l]
-        print matrix_name + '|' + str(pos) + '|' + strand + '|' + hitseq + '|'  + str(r[1])
+# for (matrix,matrix_name,result) in zip(matrices, matrix_names, results):
+#     l = len(matrix[0])
+#     for r in sorted(result, key=lambda r: r[0]):
+#         strand = r[2]
+#         pos = r[0]
+#         hitseq = seq[pos:pos+l]
+#         print matrix_name + '|' + str(pos) + '|' + strand + '|' + hitseq + '|'  + str(r[1])
 
-# for i, m in zip(range(len(results)), results):
-#     print "Hits for", i, ":", len(m)
+for i, m in zip(range(len(results)), results):
+    print "Hits for", i, ":", len(m)
 
 print "Total time:", time.clock() - total
+
+# ---- end process results ----
--- python/ex-mixing-data.py
+++ python/ex-mixing-data.py
@@ -2,45 +2,12 @@
 
 import MOODS.scan
 import MOODS.tools
+import MOODS.parsers
 
 import math
 import sys
 import time
 import os
-
-
-# helper function, loads JASPAR matrix files (.pfm)
-def load_jaspar(filename):
-    with open(filename, "r") as file_handle:
-        mat = [[float(i) for i in line.split()] for line in file_handle]
-        return mat
-
-# helper function, loads .adm files
-# returns first and last zero-order terms for log-odds computation
-def load_adm(filename):
-    with open(filename, "r") as file_handle:
-        lines = [[i for i in line.split()] for line in file_handle]
-        mat = [[float(i) for i in line[:-2]] for line in lines[:16]]
-        zero_terms = [float(line[0]) for line in lines[-4:]]
-        return (mat, zero_terms)
-
-# MOODS does not have log-odds computation for higher-order matrices yet,
-# so we'll use this quick hack
-def log_odds_dna_ho(m, bg):
-	mat = m[0]
-	zero_terms = m[1]
-	los = [[0] * len(row) for row in mat]
-
-	for i in xrange(len(mat[0])):
-		for j in xrange(16):
-			los[j][i] = math.log(mat[j][i]) - math.log(bg[j & 3])
-
-	for k in xrange(4):
-		for j in xrange(4):
-			los[(j << 2) ^ k][0] = los[(j << 2) ^ k][0] + math.log(zero_terms[j]) + math.log(bg[j])
-	return los
-
-
 
 # actual program starts here
 
@@ -62,21 +29,18 @@ matrix_directory = sys.argv[2]
 pfms = [filename for filename in os.listdir(matrix_directory) if filename[-4:] == '.pfm']
 adms = [filename for filename in os.listdir(matrix_directory) if filename[-4:] == '.adm']
 
-mz = [load_jaspar(matrix_directory + filename) for filename in pfms]
-mf = [load_adm(matrix_directory + filename) for filename in adms]
-
 # estimate background from the sequence file
 bg = MOODS.tools.bg_from_sequence_dna(seq,1)
 
 # log-odds
-lo_pfms = [MOODS.tools.log_odds(m, bg, 1) for m in mz]
-lo_adms = [log_odds_dna_ho(m, bg) for m in mf]
+lo_pfms = [MOODS.parsers.pfm_log_odds(matrix_directory + filename, bg, 1) for filename in pfms]
+lo_adms = [MOODS.parsers.adm_log_odds(matrix_directory + filename, bg, 0.0001) for filename in adms]
 matrices =  lo_pfms + lo_adms
 
 matrix_names = pfms + adms
 
 # threshold selection
-th = 0.000001
+th = 0.00001
 thresholds = [MOODS.tools.threshold_from_p(m,bg,th) for m in lo_pfms] + [MOODS.tools.threshold_from_p(m,bg,th,4) for m in lo_adms]
 
 # scanning
